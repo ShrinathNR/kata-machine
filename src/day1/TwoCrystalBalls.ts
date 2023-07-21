785fec4d230d02ee647bf4cb5db71e878e6c336f
ShrinathNR <nr.shrinath@gmail.com>
2023-07-21 23:06:30 +0530

    solved two crystal problem

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,19 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+    const j=Math.floor(Math.sqrt(breaks.length));
+    const n=Math.floor(breaks.length/j)
+    let i=0;
+    while(i<breaks.length){
+        if(breaks[i]=== true){
+            break;
+        }
+        i+=j
+        
+    }
+    i-=j;
+    for(let k=i; k<i+j; k++){
+        if(breaks[k]===true){
+            return k;
+        }
+    }
+    return -1;
 }
